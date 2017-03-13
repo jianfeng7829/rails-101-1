16f347d8d51810deeb6388996da6f06aeb129a11
jianfeng <longjianfeng7829@163.com>
2017-03-13 20:13:22 +0800

    check owner permission when access edit/update/destroy

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,4 @@
 class ApplicationController < ActionController::Base
-  before_action :authenticate_user! , only: [:new]
+  before_action :authenticate_user! , only: [:new, :create, :edit, :update, :destroy]
   protect_from_forgery with: :exception
 end
